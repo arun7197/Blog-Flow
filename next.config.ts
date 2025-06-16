f845dfbb165f4744a9ad8dd36dd3fd82a55a5ad6
arun7197 <arun.iyyappank@gmail.com>
2025-06-16 18:14:47 +0530

    deploy changes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-import withTM from 'next-transpile-modules';
-
-// ✅ 1️⃣ Add all the modules that need transpiling
-const withTranspileModules = withTM([
-  'expo-modules-core',
-  '@kinde/js-utils',
-  '@kinde-oss/kinde-typescript-sdk',
-  '@kinde-oss/kinde-auth-nextjs',
-]);
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: process.env.NODE_ENV === 'development',
@@ -21,23 +11,19 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'www.google.com',
-        port: '',
       },
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-        port: '',
       },
       {
         protocol: 'https',
         hostname: 'cdn.i-scmp.com',
-        port: '',
         pathname: '/**',
       },
     ],
@@ -49,5 +35,4 @@ const nextConfig = {
   },
 };
 
-// ✅ 2️⃣ Wrap your config:
-export default withTranspileModules(nextConfig);
+export default nextConfig;
